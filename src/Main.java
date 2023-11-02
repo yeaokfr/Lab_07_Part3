f443e799e120b870b4f52340c5c269009f6d2ff2
yeaokfr <144458128+yeaokfr@users.noreply.github.com>
2023-11-01 21:52:43 -0400

    final

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        double tempC = 0.00;
+        double tempF = 0.00;
+        boolean valid = false;
+        boolean run = true;
+        String cont = "";
+
+        while (run) {
+            System.out.println("Input your temperature in Celsius.");
+            do {
+                if (scan.hasNextDouble()) {
+                    valid = true;
+                    tempC = scan.nextDouble();
+                } else {
+                    System.out.println("Invalid input, please try again");
+                }
+                scan.nextLine();
+            } while (!valid);
+            tempF = tempC * 9 / 5 + 32;
+
+            System.out.printf("%-10s%7.2f", "Degrees C:", tempC);
+            System.out.printf("\n%-10s%7.2f", "Degrees F", tempF);
+
+            System.out.println("");
+            System.out.println("Continue? Y/N");
+            cont = scan.nextLine();
+            if (cont.equalsIgnoreCase("N")) {
+                run = false;
+            }
+        }
     }
 }
